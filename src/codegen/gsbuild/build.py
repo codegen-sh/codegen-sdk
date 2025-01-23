46d22cfed98b1b594279f7694cc83ebd6a44b8cc
Edward Li <jiaxi.edwardli@gmail.com>
2025-01-23 15:19:54 -0800

    Move graph_sitter_build into codebase.gsbuild

--- src/codegen/gsbuild/build.py
+++ src/codegen/gsbuild/build.py
@@ -16,7 +16,7 @@ def update_init_file(file: Path) -> None:
 
 
 class SpecialBuildHook(BuildHookInterface):
-    PLUGIN_NAME = "graph_sitter_build"
+    PLUGIN_NAME = "codegen_build"
 
     def initialize(self, version: str, build_data: dict[str, Any]) -> None:
         file = Path(self.root) / "src" / "codegen" / "sdk" / "__init__.py"
