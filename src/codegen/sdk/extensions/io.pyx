ac6d73137062c9c9b18d6da0a30f0492a72bf203
bagel897 <ellenagarwal897@gmail.com>
2025-01-27 13:16:00 -0800

    don't create parent dir

--- src/codegen/sdk/extensions/io.pyx
+++ src/codegen/sdk/extensions/io.pyx
@@ -3,15 +3,10 @@ from concurrent.futures import ThreadPoolExecutor
 import os
 
 
-def write_file(file_path: Path, content: bytes):
-    file_path.parent.mkdir(parents=True, exist_ok=True)
-    file_path.write_bytes(content)
-
-
 def write_changes(files_to_remove: list[Path], files_to_write: list[tuple[Path, bytes]]):
     # Start at the oldest sync and then apply non-conflicting newer changes
     with ThreadPoolExecutor() as executor:
         for file_to_remove in files_to_remove:
             executor.submit(os.remove, file_to_remove)
         for file_to_write, content in files_to_write:
-            executor.submit(write_file, file_to_write, content)
+            executor.submit(file_to_write.write_bytes, content)
