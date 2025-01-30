25429232ae409119ccc020bf01a2678731b7fbc2
tomcodegen <tkucar@codegen.com>
2025-01-29 17:53:40 -0800

    merge

--- tests/unit/codegen/sdk/python/file/test_file_complex_example_test_spliter.py
+++ tests/unit/codegen/sdk/python/file/test_file_complex_example_test_spliter.py
@@ -1,4 +1,4 @@
-from graph_sitter.codebase.factory.get_session import get_codebase_session
+from codegen.sdk.codebase.factory.get_session import get_codebase_session
 
 
 def test_file_complex_example_test_spliter(tmpdir) -> None:
