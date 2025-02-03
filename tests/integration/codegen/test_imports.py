856fe01186db141567339fde686f2451cc24ae23
bagel897 <ellenagarwal897@gmail.com>
2025-02-03 10:56:02 -0800

    Fix test

--- tests/integration/codegen/test_imports.py
+++ tests/integration/codegen/test_imports.py
@@ -1,5 +1,6 @@
 import codegen
 from codegen import Codebase
+from codegen.sdk.code_generation.current_code_codebase import get_codegen_codebase_base_path
 
 
 def test_codegen_imports():
@@ -11,6 +12,5 @@ def test_codegen_imports():
     # Test class
     cls = codegen.Function
     assert cls is not None
-
-    codebase = Codebase("./")
+    codebase = Codebase(get_codegen_codebase_base_path())
     assert codebase is not None
