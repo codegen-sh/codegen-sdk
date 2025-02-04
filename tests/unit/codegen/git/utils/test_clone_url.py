e17a8ee5a67c3bd4a3c1f5331effe46807be3450
Christine Wang <christine@codegen.com>
2025-02-03 22:42:17 -0800

    add tests

--- tests/unit/codegen/git/utils/test_clone_url.py
+++ tests/unit/codegen/git/utils/test_clone_url.py
@@ -8,8 +8,10 @@ from codegen.git.utils.clone_url import add_access_token_to_url
     [
         ("https://github.com/owner/repo.git", "token", "https://x-access-token:token@github.com/owner/repo.git"),  # https url with token
         ("https://github.com/owner/repo.git", None, "https://github.com/owner/repo.git"),  # https url without token
+        ("https://github.com/owner/repo.git", "", "https://github.com/owner/repo.git"),  # https url with empty token
         ("github.com/owner/repo", "token", "https://x-access-token:token@github.com/owner/repo"),  # scheme missing with token
         ("github.com/owner/repo", None, "https://github.com/owner/repo"),  # scheme missing without token
+        ("github.com/owner/repo", "", "https://github.com/owner/repo"),  # scheme missing with empty token
     ],
 )
 def test_add_access_token_to_url(url, token, expected):
