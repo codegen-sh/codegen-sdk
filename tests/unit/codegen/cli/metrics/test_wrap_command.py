0c5f97dc6f7ead3177c7181573f835c886d62fa4
Chris Lee <clee@codegen.com>
2025-02-04 15:55:14 -0800

    add a test for command with args

--- tests/unit/codegen/cli/metrics/test_wrap_command.py
+++ tests/unit/codegen/cli/metrics/test_wrap_command.py
@@ -1,5 +1,5 @@
 import pytest
-from rich_click import command
+from rich_click import argument, command, option
 
 from codegen.cli.metrics.wrap_command import metrics_wrapper
 
@@ -13,6 +13,23 @@ def test_metrics_wrapper_success(mock_metrics_client):
     result = wrapped_command([], standalone_mode=False)
 
     assert result == "success"
+    mock_metrics_client.capture_event.assert_called_once_with(
+        "command test command succeeded",
+        {"duration": pytest.approx(0, abs=1e-2)},  # Allowing a small margin for time measurement
+    )
+
+
+def test_metrics_wrapper_with_args(mock_metrics_client):
+    @command("test command")
+    @argument("arg1")
+    @option("--arg2", is_flag=True, default=False)
+    def test_command(arg1, arg2=False):
+        return f"success {arg1} {arg2}"
+
+    wrapped_command = metrics_wrapper(test_command)
+    result = wrapped_command(["arg1", "--arg2"], standalone_mode=False)
+
+    assert result == "success arg1 True"
     mock_metrics_client.capture_event.assert_called_once_with(
         "command test command succeeded",
         {"duration": pytest.approx(0, abs=1e-2)},  # Allowing a small margin for time measurement
