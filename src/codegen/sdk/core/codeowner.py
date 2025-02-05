2cf9a363614ba383c8af960daf54f7889150c66f
Chris Lee <clee@codegen.com>
2025-02-05 15:59:41 -0800

    add: fuller directory API refactor

--- src/codegen/sdk/core/codeowner.py
+++ src/codegen/sdk/core/codeowner.py
@@ -1,33 +1,29 @@
+import logging
 from collections.abc import Iterable, Iterator
-from itertools import chain
-from typing import TYPE_CHECKING, Callable, Generic, Literal, ParamSpec, TypeVar
+from typing import TYPE_CHECKING, Callable, Literal, TypeVar
 
 from codeowners import CodeOwners as CodeOwnersParser
 
 from codegen.git.utils.cache_utils import cached_generator
+from codegen.sdk.core.interfaces.files_interface import FilesInterface, FilesParam, TFile
 from codegen.shared.decorators.docs import apidoc
 
 if TYPE_CHECKING:
-    from codegen.sdk.core.file import SourceFile
     from codegen.sdk.core.symbol import Symbol
-import logging
 
 logger = logging.getLogger(__name__)
 
 
-TSourceFile = TypeVar("TSourceFile", bound="SourceFile")
-SourceParam = ParamSpec("SourceParam")
 TSymbol = TypeVar("TSymbol", bound="Symbol")
 
 
 @apidoc
-class CodeOwner(Generic[TSourceFile]):
+class CodeOwner(FilesInterface):
     """CodeOwner is a class that represents a code owner in a codebase.
 
     It is used to iterate over all files that are owned by a specific owner.
 
     Attributes:
-        parser: The CodeOwnersParser that was used to parse the codeowners file.
         owner_type: The type of the owner (USERNAME, TEAM, EMAIL).
         owner_value: The value of the owner.
         files_source: A callable that returns an iterable of all files in the codebase.
@@ -35,20 +31,21 @@ class CodeOwner(Generic[TSourceFile]):
 
     owner_type: Literal["USERNAME", "TEAM", "EMAIL"]
     owner_value: str
-    files_source: Callable[SourceParam, Iterable[TSourceFile]]
+    files_source: Callable[FilesParam, Iterable[TFile]]
 
-    def __init__(self, files_source: Callable[SourceParam, Iterable[TSourceFile]], owner_type: Literal["USERNAME", "TEAM", "EMAIL"], owner_value: str):
+    def __init__(self, files_source: Callable[FilesParam, Iterable[TFile]], owner_type: Literal["USERNAME", "TEAM", "EMAIL"], owner_value: str):
         self.owner_type = owner_type
         self.owner_value = owner_value
         self.files_source = files_source
+        self.files = self.files_generator
 
     @classmethod
-    def from_parser(cls, parser: CodeOwnersParser, file_source: Callable[SourceParam, Iterable[TSourceFile]]) -> list["CodeOwner"]:
+    def from_parser(cls, parser: CodeOwnersParser, file_source: Callable[FilesParam, Iterable[TFile]]) -> list["CodeOwner"]:
         """Create a list of CodeOwner objects from a CodeOwnersParser.
 
         Args:
             parser (CodeOwnersParser): The CodeOwnersParser to use.
-            file_source (Callable[SourceParam, Iterable[TSourceFile]]): A callable that returns an iterable of all files in the codebase.
+            file_source (Callable[FilesParam, Iterable[TFile]]): A callable that returns an iterable of all files in the codebase.
 
         Returns:
             list[CodeOwner]: A list of CodeOwner objects.
@@ -60,23 +57,18 @@ class CodeOwner(Generic[TSourceFile]):
         return codeowners
 
     @cached_generator(maxsize=16)
-    def files(self, *args: SourceParam.args, **kwargs: SourceParam.kwargs) -> Iterable[TSourceFile]:
+    def files_generator(self, *args: FilesParam.args, **kwargs: FilesParam.kwargs) -> Iterable[TFile]:
         for source_file in self.files_source(*args, **kwargs):
             # Filter files by owner value
             if self.owner_value in source_file.owners:
                 yield source_file
 
     @property
-    def symbols(self) -> list[TSymbol]:
-        """Get a recursive list of all symbols in the directory and its subdirectories."""
-        return list(chain.from_iterable(f.symbols for f in self.files))
-
-    @property
     def name(self) -> str:
         return self.owner_value
 
-    def __iter__(self) -> Iterator[TSourceFile]:
-        return self.files()
+    def __iter__(self) -> Iterator[TFile]:
+        return self.files_generator()
 
     def __repr__(self) -> str:
         return f"CodeOwner(owner_type={self.owner_type}, owner_value={self.owner_value})"
