6486bb68c5125da533c893a383f7d614d56df6bf
bagel897 <ellenagarwal897@gmail.com>
2025-02-07 09:56:28 -0800

    remove tmp.rs

--- sdk-core/codegen-sdk-cst-generator/src/generator/format.rs
+++ sdk-core/codegen-sdk-cst-generator/src/generator/format.rs
@@ -7,9 +7,6 @@ use prettyplease;
 use syn;
 
 pub fn format_cst(cst: &str) -> String {
-    let file = File::create("tmp.rs").unwrap();
-    let mut writer = BufWriter::new(file);
-    writer.write_all(cst.as_bytes()).unwrap();
     let parsed = syn::parse_str::<syn::File>(&cst)
         .map_err(|e| {
             println!("{:#?}", e);
