5af8556509bc90110f199f7cca8c9fb6b333f4c5
caroljung-cg <165736129+caroljung-cg@users.noreply.github.com>
2025-02-11 22:10:38 +0000

    Automated pre-commit update

--- src/codegen/runner/sandbox/runner.py
+++ src/codegen/runner/sandbox/runner.py
@@ -13,7 +13,6 @@ from codegen.sdk.codebase.factory.codebase_factory import CodebaseType
 from codegen.sdk.core.codebase import Codebase
 from codegen.shared.compilation.string_to_code import create_execute_function_from_codeblock
 from codegen.shared.configs.config import config
-from codegen.shared.enums.programming_language import ProgrammingLanguage
 from codegen.shared.performance.stopwatch_utils import stopwatch
 
 logger = logging.getLogger(__name__)
